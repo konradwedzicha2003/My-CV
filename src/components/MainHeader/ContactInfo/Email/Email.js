17b87464ca668dccb72ad8de49f530a96b8840b4
Konrad Wedzicha <progmaster2003@gmail.com>
2023-05-06 23:25:18 +0200

    added animations

--- src/components/MainHeader/ContactInfo/Email/Email.js
+++ src/components/MainHeader/ContactInfo/Email/Email.js
@@ -6,11 +6,12 @@ import email from "../../../../assets/email.png";
 const Email = () => {
 
     return (
-        <div className="contact-info__content-box">
+        <div className="contact-info__content-box email">
             <div className="contact-info__icon">
                 <img src={email} alt="email"/>
             </div>
             <div className="contact-info__email">
+                <div className="contact-info__underline"/>
                 <a href="mailto:konradw2003@interia.pl">konradw2003@interia.pl</a>
             </div>
         </div>
